fed8a38211c890ffa3b71fed84164e005f9cbad6
邓超 <dc870638530@163.com>
2020-01-02 18:02:29 +0800

    --

--- src/components/MyCart.vue
+++ src/components/MyCart.vue
@@ -5,10 +5,14 @@
                 <th>课程名称</th>
                 <th>课程单价</th>
                 <th>数量</th>
+                <th>单项总价</th>
                 <th>总价</th>
             </tr>
-            <tr>
-
+            <tr v-for='(item, index) in courseList'>
+                <td>{{ item.name }}</td>
+                <td>{{ item.price }}</td>
+                <td><button @click='reduceNum(item,index)'>-</button>{{ item.num }}<button @click='addNum(item)'>+</button></td>
+                <td>{{ item.price*item.num }}</td>
             </tr>
         </table>
     </div>
@@ -16,7 +20,20 @@
 
 <script>
     export default {
-        props:['courseList']
+        props:['courseList'],
+        methods: {
+            reduceNum(item,index){
+                console.log(item.num)
+                if(item.num >= 1){
+                    item.num -= 1
+                }else {
+                    alert('已经到0啦，需要删除吗？')
+                }
+            },
+            addNum(item,index){
+                item.num += 1
+            }
+        }
     }
 </script>
 
--- src/views/Shopcart.vue
+++ src/views/Shopcart.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
     <h1>购物车</h1>
-    <hr />
+    <hr>
     <div>
       <div>
         <h2>添加课程</h2>
-        <label for>课程名称: </label>
-        <input type="text" v-model='courseInfo.name'/>
+        <label for>课程名称:</label>
+        <input type="text" v-model="courseInfo.name">
       </div>
       <div>
-        <label for>课程价格: </label>
-        <input type="text" v-model='courseInfo.price'/>
+        <label for>课程价格:</label>
+        <input type="text" v-model="courseInfo.price">
       </div>
-      <div><button @click='addCourseToList'>添加课程到购物车</button></div>
+      <div>
+        <button @click="addCourseToList">添加到课程列表</button>
+      </div>
       <div>
         <h2>课程列表</h2>
         <table>
@@ -27,48 +29,63 @@
         </table>
       </div>
       <div>
-        <my-cart></my-cart>
+        <my-cart :courseList='courseList'></my-cart>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import MyCart from '../components/MyCart'
+import MyCart from "../components/MyCart";
 export default {
   name: "shopcart",
-  components:{
+  components: {
     MyCart
   },
   data() {
     return {
-      courseInfo:{
-        name:'',
-        price:''
+      courseInfo: {
+        name: "",
+        price: "",
+        num:0
       },
       courseList: [
         {
           name: "Python",
-          price: 100
+          price: 100,
+        num:0
+
         },
         {
           name: "PHP",
-          price: 200
+          price: 200,
+        num:0
+
         },
         {
           name: "Java",
-          price: 300
+          price: 300,
+        num:0
+
         }
       ]
     };
   },
   methods: {
-    addCourseToList(){
-      this.courseList.push(this.courseInfo)
+    addCourseToList() {
+      if (this.courseInfo.name !== "" && this.courseInfo.price !== "") {
+        this.courseList.push(this.courseInfo);
+        // this.courseInfo.name=''
+        // this.courseInfo.price=''
+      }else {
+        alert('添加课程不能为空！')
+      }
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+
+
 </style>
